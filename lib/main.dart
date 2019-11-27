1bf35f2ddf6819941bcf961e1058798be411b218
grayHatEnigma <salama92work@gmail.com>
2019-11-27 08:56:40 +0200

    app version 2 - shake

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shake/shake.dart';
 
 void main() {
   return runApp(MyApp());
@@ -37,6 +38,7 @@ class _DicePageState extends State<DicePage> {
   Random rand = Random();
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -45,28 +47,33 @@ class _DicePageState extends State<DicePage> {
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                print('left dice is clicked');
-                setState(() {
-                  leftDiceNumber = rand.nextInt(5) + 1;
-                });
-              },
+              onPressed: pressed,
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
           ),
           Expanded(
             child: FlatButton(
-              onPressed: () {
-                print('right dice is clicked');
-                setState(() {
-                  rightDiceNumber = rand.nextInt(5) + 1;
-                });
-              },
+              onPressed: pressed,
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void pressed() {
+    setState(() {
+      leftDiceNumber = rand.nextInt(6) + 1;
+      rightDiceNumber = rand.nextInt(6) + 1;
+    });
+  }
+
+  void initState() {
+    super.initState();
+    ShakeDetector detector = ShakeDetector.autoStart(onPhoneShake: () {
+      pressed();
+      print("Phone is shaking!");
+    });
   }
 }
